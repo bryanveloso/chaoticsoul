6d0caf4a31bb102aa02610a7c6a99695a2c3e846
Bryan Veloso <bryan@revyver.com>
2008-08-07 15:09:01 -0700

    Forgot to correctly rev the version number.

--- footer.php
+++ footer.php
@@ -2,7 +2,7 @@
 
 <div class="hr">&nbsp;</div>
 <div id="footer">
-    <p>Get a free blog at <a href="http://wordpress.com/">WordPress.com</a> and more themes at <a href="http://labs.revyver.com">Revyver Labs</a>. Theme: ChaoticSoul (v1.2.1) by <a href="http://avalonstar.com" rel="designer">Bryan Veloso</a>.</p>
+    <p>Get a free blog at <a href="http://wordpress.com/">WordPress.com</a> and more themes at <a href="http://labs.revyver.com">Revyver Labs</a>. Theme: ChaoticSoul (v1.3) by <a href="http://avalonstar.com" rel="designer">Bryan Veloso</a>.</p>
 </div>
 </div>
 
