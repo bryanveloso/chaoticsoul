621b1a5c0d5baca7bc9290733e1814589dadb4d3
Bryan Veloso <bryan@revyver.com>
2008-11-18 02:17:06 -0800

    Missing a few more semicolons. :(

--- single.php
+++ single.php
@@ -5,12 +5,12 @@
     <?php if (have_posts()) : while (have_posts()) : the_post(); ?>
         
         <div class="post" id="post-<?php the_ID(); ?>">
-            <h2 class="title"><a href="<?php echo get_permalink() ?>" rel="bookmark" title="Permanent Link: <?php the_title(); ?>"><?php the_title(); ?></a></h2>
+            <h2 class="title"><a href="<?php echo get_permalink(); ?>" rel="bookmark" title="Permanent Link: <?php the_title(); ?>"><?php the_title(); ?></a></h2>
             
             <div class="entrytext">
                 <?php the_content('<p class="serif">Read the rest of this entry &raquo;</p>'); ?>
                 <?php link_pages('<p><strong>Pages:</strong> ', '</p>', 'number'); ?>
-                <p class="authormeta">~ by <?php the_author() ?> on <? the_date() ?>.</p>
+                <p class="authormeta">~ by <?php the_author() ?> on <? the_date(); ?>.</p>
                 <?php if (the_category(', ')); ?><p class="postmetadata">Posted in <?php the_category(', ') ?> | <?php edit_post_link('Edit', '', ' | '); ?></p><?php endif; ?>
                 <?php if (get_the_tags()); ?><p class="postmetadata"><?php the_tags(); ?></p><?php endif; ?>
             </div>
